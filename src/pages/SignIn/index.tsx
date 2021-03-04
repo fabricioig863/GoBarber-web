d283ab5a37d36c44339cbc63064759d7ce098d88
Fabricio <fabricioig863@gmail.com>
2021-03-04 20:15:17 -0300

    alterações no prettier

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -5,6 +5,9 @@ import logoImg from "../../assets/logo.svg";
 
 import { Container, Content, Background } from "./styles";
 
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+
 const SignIn: React.FC = () => (
   <Container>
     <Content>
@@ -13,11 +16,11 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça seu logon</h1>
 
-        <input placeholder="E-mail" />
+        <Input name="email" placeholder="E-mail" />
 
-        <input type="password" placeholder="Senha" />
+        <Input name="password" type="password" placeholder="Senha" />
 
-        <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
       </form>
