574cae77f565f1c9ab79960e5bbada32cfa18cd8
Fabricio <fabricioig863@gmail.com>
2021-04-01 14:12:52 -0300

    Front-end finalizado

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -34,6 +34,8 @@ const AuthProvider: React.FC = ({ children }) => {
     const user = localStorage.getItem('@GoBarber:user');
 
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
       return { token, user: JSON.parse(user) };
     }
 
@@ -72,7 +74,7 @@ const AuthProvider: React.FC = ({ children }) => {
         user,
       });
     },
-    [setData, data.token],
+    [data.token],
   );
 
   return (
